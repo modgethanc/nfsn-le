982414d2c3e7caab8b895928fab65e2d53540805
Alexander Harkness <me@bearbin.net>
2015-12-27 21:12:02 +0000

    Get chain cert also.

--- new_cert.sh
+++ new_cert.sh
@@ -11,6 +11,7 @@ openssl genrsa 4096 > $1.key
 openssl req -new -sha256 -key $1.key -subj "/CN=$1" > $1.csr
 
 mkdir -p /home/public/.well-known/acme-challenge
+curl -s -o $1.chn https://letsencrypt.org/certs/lets-encrypt-x1-cross-signed.pem
 
 python sign_csr.py --public-key user.pub --email $2 $1.csr > $1.crt
 
