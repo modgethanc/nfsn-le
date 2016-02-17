2e977adf4a0e4dd8f455a1ceb17c05be2a2a2e89
Alexander Harkness <me@bearbin.net>
2016-02-17 16:22:24 +0000

    Update new_cert.sh

--- new_cert.sh
+++ new_cert.sh
@@ -13,6 +13,6 @@ openssl req -new -sha256 -key $1.key -subj "/CN=$1" > $1.csr
 mkdir -p /home/public/.well-known/acme-challenge
 curl -s -o $1.chn https://letsencrypt.org/certs/lets-encrypt-x1-cross-signed.pem
 
-python sign_csr.py --public-key user.pub --email $2 $1.csr > $1.crt
+python /home/protected/ssl/nfsn-le/sign_csr.py --public-key user.pub --email $2 $1.csr > $1.crt
 
 rm $1.csr *.json *.sig /home/public/.well-known/acme-challenge/*
