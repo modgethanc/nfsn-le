553fde564ae3e602d1396b50316061e4cbe3f75d
Alexander Harkness <me@bearbin.net>
2016-02-17 16:21:53 +0000

    Update renew_cert.sh

--- renew_cert.sh
+++ renew_cert.sh
@@ -6,7 +6,7 @@ set -e
 #
 # ./renew_cert.sh example.com me@example.com
 
-pushd /home/protected/ssl
+cd /home/protected/ssl
 
 # Check if certificate is in need of renewal.
 if openssl x509 -checkend 2592000 -noout -in $1.crt
@@ -17,6 +17,5 @@ else
 	# Certificate has less than one month to go, renewal is needed.
 	mv $1.crt $1.crt.bak
 	mv $1.key $1.key.bak
-	popd
-	./new_cert.sh $1 $2
+	/home/protected/ssl/nfsn-le/new_cert.sh $1 $2
 fi
