24fa556a9332bfe7985fedeb3d612b9ead524e43
Alexander Harkness <me@bearbin.net>
2016-05-07 15:42:54 +0100

    fixed wrong path.

--- new_cert.sh
+++ new_cert.sh
@@ -19,8 +19,8 @@ mkdir -p /home/public/.well-known/acme-challenge
 if [ -z "$2" ]
 then
 	echo "Using anonymous registration."
-	python /home/protected/ssl/nfsn-le/sign_csr.py --public-key user.pub $1.csr > "/home/protected/ssl/$1.crt"
+	python ./sign_csr.py --public-key user.pub $1.csr > "/home/protected/ssl/$1.crt"
 else
-	python /home/protected/ssl/nfsn-le/sign_csr.py --public-key user.pub --email $2 $1.csr > "/home/protected/ssl/$1.crt"
+	python ./sign_csr.py --public-key user.pub --email $2 $1.csr > "/home/protected/ssl/$1.crt"
 fi
 rm -rf $1.csr *.json *.sig /home/public/.well-known
