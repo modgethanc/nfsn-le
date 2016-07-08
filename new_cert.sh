c865497037a1d4aa567040b488a1cb08e26eec2d
Alexander Harkness <me@bearbin.net>
2016-07-08 14:03:13 +0100

    extraneous slash

--- new_cert.sh
+++ new_cert.sh
@@ -16,6 +16,6 @@ openssl genrsa 4096 > "certs/$1.key"
 openssl req -new -sha256 -key "certs/$1.key" -subj "/CN=$1" > $1.csr
 
 mkdir -p /home/public/.well-known/acme-challenge
-python ./sign_csr.py --public-key user.pub --email "$2" "$1.csr" > "/certs/$1.crt"
+python ./sign_csr.py --public-key user.pub --email "$2" "$1.csr" > "certs/$1.crt"
 rm -rf $1.csr *.json *.sig /home/public/.well-known
 echo "'cat certs/*' for info to supply to nfsn"
