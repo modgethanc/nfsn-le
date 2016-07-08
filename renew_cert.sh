89f1c1e2a8b371f2de49d455f3b3f1d6cdb40568
Alexander Harkness <me@bearbin.net>
2016-07-08 14:13:03 +0100

    new certsdir

--- renew_cert.sh
+++ renew_cert.sh
@@ -6,17 +6,20 @@ set -e
 #
 # ./renew_cert.sh example.com me@example.com
 
+$certsdir = "/home/protected/certs"
+$backupdir = "/home/protected/certbackup"
+
 cd /home/private/nfsn-le
 
 # Check if certificate is in need of renewal.
-if openssl x509 -checkend 3000000 -noout -in "certs/$1.crt"
+if openssl x509 -checkend 3000000 -noout -in "$certsdir/$1.crt"
 then
 	# Certificate is still valid.
 	exit 0
 else
 	echo "Certificate for $1 is in need of renewal".
-	mkdir -p backups
-	mv "certs/$1.crt" "backups/$1.crt"
-	mv "certs/$1.key" "backups/$1.key"
+	mkdir -p $backupdir
+	mv "$certsdir/$1.crt" "backups/$1.crt"
+	mv "$certsdir/$1.key" "backups/$1.key"
 	./new_cert.sh "$1" "$2"
 fi
