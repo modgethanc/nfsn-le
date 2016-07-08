ccc2beb0854e46a77a85c8d7858e2150e83dd270
Alexander Harkness <me@bearbin.net>
2016-07-08 14:11:36 +0100

    new certsdir

--- new_cert.sh
+++ new_cert.sh
@@ -7,15 +7,17 @@ set -e
 # ./new_cert.sh example.com me@example.com
 # user.key and user.pub already exist in the current directory.
 
+$certsdir = "/home/protected/certs"
+
 cd /home/private/nfsn-le
-mkdir -p /home/private/nfsn-le/certs
-curl -s -o "certs/$1.chn" https://letsencrypt.org/certs/lets-encrypt-x3-cross-signed.pem
+mkdir -p $certsdir
+curl -s -o "$certsdir/$1.chn" https://letsencrypt.org/certs/lets-encrypt-x3-cross-signed.pem
 
 # Generate the certificate.
-openssl genrsa 4096 > "certs/$1.key"
-openssl req -new -sha256 -key "certs/$1.key" -subj "/CN=$1" > $1.csr
+openssl genrsa 4096 > "$certsdir/$1.key"
+openssl req -new -sha256 -key "$certsdir/$1.key" -subj "/CN=$1" > $1.csr
 
 mkdir -p /home/public/.well-known/acme-challenge
-python ./sign_csr.py --public-key user.pub --email "$2" "$1.csr" > "certs/$1.crt"
+python ./sign_csr.py --public-key user.pub --email "$2" "$1.csr" > "$certsdir/$1.crt"
 rm -rf $1.csr *.json *.sig /home/public/.well-known
-cat "certs/*" | nfsn -i set-tls
+cat "$certsdir/*" | nfsn -i set-tls
